8f60645f7d6005aa7c8d14f85d8c7f3d6fccba49
Heine Günther <heine_gunther@hotmail.com>
2024-01-10 22:08:22 +0100

    Chsnged app.js

--- src/App.js
+++ src/App.js
@@ -14,61 +14,59 @@ import UpdateProfilePage from "./pages/UpdateProfilePage";
 import ProfilesPage from "./pages/ProfilesPage";
 import SingleProfilePage from "./pages/SingleProfilePage";
 import NotFoundPage from "./pages/NotFoundPage";
-import { AuthProvider, useAuth } from "./context/AuthContext";
+import { useAuth } from "./context/AuthContext";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./common/theme/theme";
 
 function App() {
   const auth = useAuth();
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <div className="App">
-            <Navbar auth={auth} />
-            <Container>
-              <div id="page-body" style={{ marginBottom: "80px" }}>
-                <Routes>
-                  <Route path="/" element={<LandingPage />} />
-                  <Route path="/login" element={<LoginPage auth={auth} />} />
-                  <Route
-                    path="/register"
-                    element={<RegisterPage auth={auth} />}
-                  />
-                  <Route path="/posts" element={<PostsPage auth={auth} />} />
-                  <Route
-                    path="/posts/:postId"
-                    element={<SinglePostPage auth={auth} />}
-                  />
-                  <Route
-                    path="/newPost"
-                    element={<CreatePostPage auth={auth} />}
-                  />
-                  <Route
-                    path="/myprofile"
-                    element={<ProfilePage auth={auth} />}
-                  />
-                  <Route
-                    path="/myprofile/:myprofileId"
-                    element={<UpdateProfilePage auth={auth} />}
-                  />
-                  <Route
-                    path="/profiles"
-                    element={<ProfilesPage auth={auth} />}
-                  />
-                  <Route
-                    path="/profiles/:profileName"
-                    element={<SingleProfilePage auth={auth} />}
-                  />
-                  <Route path="*" element={<NotFoundPage auth={auth} />} />
-                </Routes>
-                <Footer />
-              </div>
-            </Container>
-          </div>
-        </ThemeProvider>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Navbar auth={auth} />
+          <Container>
+            <div id="page-body" style={{ marginBottom: "80px" }}>
+              <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<LoginPage auth={auth} />} />
+                <Route
+                  path="/register"
+                  element={<RegisterPage auth={auth} />}
+                />
+                <Route path="/posts" element={<PostsPage auth={auth} />} />
+                <Route
+                  path="/posts/:postId"
+                  element={<SinglePostPage auth={auth} />}
+                />
+                <Route
+                  path="/newPost"
+                  element={<CreatePostPage auth={auth} />}
+                />
+                <Route
+                  path="/myprofile"
+                  element={<ProfilePage auth={auth} />}
+                />
+                <Route
+                  path="/myprofile/:myprofileId"
+                  element={<UpdateProfilePage auth={auth} />}
+                />
+                <Route
+                  path="/profiles"
+                  element={<ProfilesPage auth={auth} />}
+                />
+                <Route
+                  path="/profiles/:profileName"
+                  element={<SingleProfilePage auth={auth} />}
+                />
+                <Route path="*" element={<NotFoundPage auth={auth} />} />
+              </Routes>
+              <Footer />
+            </div>
+          </Container>
+        </div>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
